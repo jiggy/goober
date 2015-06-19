15306dea84a7b5dd3a6d08555eb3e82b8abbded1
Josh Gitlin <jgitlin@hugeinc.com>
2015-06-19 17:23:36 -0400

    add command line param for port

--- src/main/java/mrserver/MrServer.java
+++ src/main/java/mrserver/MrServer.java
@@ -1,15 +1,14 @@
+package mrserver;
 
 import io.undertow.Handlers;
 import io.undertow.Undertow;
 import io.undertow.UndertowOptions;
-import io.undertow.attribute.ExchangeAttributes;
-import io.undertow.attribute.RequestURLAttribute;
-import io.undertow.predicate.Predicates;
 import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
 import io.undertow.server.handlers.resource.ClassPathResourceManager;
-import io.undertow.util.HttpString;
 import io.undertow.util.Methods;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
 
 import java.io.IOException;
 import java.security.*;
@@ -18,13 +17,23 @@ import java.security.cert.CertificateException;
 import javax.net.ssl.*;
 
 public class MrServer {
-    public static void main(String[] args) throws Exception {
 
+    @Option(name="-p", usage = "Set the port number.")
+    private int port = 8888;
+
+    public static void main(String[] args) throws Exception{
+        new MrServer(args);
+    }
+
+    public MrServer(String[] args) throws Exception {
+        CmdLineParser p = new CmdLineParser(this);
+        p.parseArgument(args);
+System.out.println("Starting on port " + port);
         SSLContext sslContext = SSLContext.getInstance("TLS");
         sslContext.init(getKeyManagers(), null, null);
         Undertow.builder()
                 .setServerOption(UndertowOptions.ENABLE_HTTP2, true)
-                .addHttpListener(8080, "localhost")
+                .addHttpListener(port, "localhost")
                 .addHttpsListener(8081, "localhost", sslContext)
                 .setHandler(new HttpHandler() {
                                 @Override
